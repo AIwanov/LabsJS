00efe1bdace5b47e6c69877c4b03dfcc9d8050b9
Ivanov Andrey <aivanov@lanbilling.ru>
2016-03-30 09:27:55 +0300

    Lab.1 Добавил разбор xml-файла, реализовал возможность вывода элемента списка в интерфейс

---
+++ LabsJS/Lab_1/comp_lab1/WebApplication1/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="comp_lab1._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--<script runat="server">
+protected void get_list()
+{
+    for (int i = 0; i < data.parameters.Count; i++)
+    {
+        dataGrid1.Items[i].Cells[0].Text = data.parameters[i].name;
+        dataGrid1.Items[i].Cells[1].Text = data.parameters[i].description;
+        dataGrid1.Items[i].Cells[2].Text = data.parameters[i].value;
+    }
+}
+</script>  -->
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <!--<asp:DataGrid id="dataGrid1" runat="server"></asp:DataGrid>-->
+        <div>
+            <asp:TextBox ID="name" runat="server" Text="<%# data.parameters[0].parName %>" />
+            <asp:TextBox ID="description" runat="server" Text="<%# data.parameters[0].parDescription %>" />
+            <asp:TextBox ID="value" runat="server" Text="<%# data.parameters[0].parValue %>" />
+        </div>
+        <div>
+            <asp:TextBox ID="name1" runat="server" Text="<%# data.parameters[1].parName %>" />
+            <asp:TextBox ID="description1" runat="server" Text="<%# data.parameters[1].parDescription %>" />
+            <asp:TextBox ID="value1" runat="server" Text="<%# data.parameters[1].parValue %>" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ LabsJS/Lab_1/comp_lab1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Serialization;
+
+namespace comp_lab1
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        [XmlRoot("Parameters")]
+        public class Parameters
+        {
+            [XmlElement("Parameter")]
+            public List<Parameter> parameters;
+        }
+        public class Parameter
+        {
+            [XmlElement("Name")]
+            public string name;
+            [XmlElement("Description")]
+            public string description;
+            [XmlElement("Value")]
+            public string value;
+
+            public Parameter()
+            {
+                name = "";
+                description = "";
+                value = "";
+            }
+
+            public Parameter(string _name, string _description, string _value)
+            {
+                name = _name;
+                description = _description;
+                value = _value;
+            }
+            public string parName
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public string parDescription
+            {
+                get { return description; }
+                set { description = value; }
+            }
+            public string parValue
+            {
+                get { return this.value; }
+                set { this.value = value; }
+            }
+        }
+
+        public Parameters data;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Parameters));
+
+            FileStream fs = new FileStream("Input.xml", FileMode.Open);
+
+            XmlReader reader = XmlReader.Create(fs);
+
+            data = (Parameters)serializer.Deserialize(reader);
+            fs.Close();
+
+            this.DataBind();
+        }
+    }
+}
